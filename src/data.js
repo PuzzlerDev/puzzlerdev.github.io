4aa73fe6c354ef4254861ccadcbdc2f3c3c8ceca
Adonys Santos <adonyssantosjimenez@gmail.com>
2021-08-26 11:24:21 -0400

    Update data.js

--- src/data.js
+++ src/data.js
@@ -5,7 +5,7 @@ const data = {
       alt: 'PuzzlerDev logo',
     },
     fullname: 'Puzzler Dev',
-    title: 'Dev community open source',
+    title: 'Open Source Development Community.',
   },
   links: [
     {
@@ -13,6 +13,17 @@ const data = {
       link: 'https://github.com/puzzlerdev',
       openInNewTab: true,
     },
+    {
+      title: 'Discord',
+      link: 'https://discord.gg/N88xjqcRNz',
+      openInNewTab: true,
+    },
+    {
+      title: 'Telegram',
+      link: 'https://t.me/puzzlerdev',
+      openInNewTab: true,
+    },
+
     {
       title: 'TikTok',
       link: 'https://tiktok.com/@puzzlerdev',
@@ -24,14 +35,14 @@ const data = {
       openInNewTab: true,
     },
     {
-      title: 'Twitter',
-      link: 'https://twitter.com/puzzlerdev',
+      title: 'YouTube',
+      link: 'https://www.youtube.com/channel/UCMsGIPLBFLGC2kuh-8QIA2Q',
       openInNewTab: true,
     },
     {
-      title: 'Work Connect (Discord Community)',
-      link: 'https://work-connect.github.io/',
-      openInNewTab: false,
+      title: 'Twitter',
+      link: 'https://twitter.com/puzzlerdev',
+      openInNewTab: true,
     },
   ],
 };
